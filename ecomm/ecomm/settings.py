38d52cba8ba6b6af9d8a80a48bd62a6f62e6c451
Henry Wanjiru <haronkuria2015@gmail.com>
2025-12-25 13:02:54 +0300

    main mobile menu responsive menu added

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = 'django-insecure-mfvte4i@l!m)ly9#%rqo^j7d8*0*z#tjw)qd^+(7220z#7mbwi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["27328ea08304.ngrok-free.app","localhost","192.168.100.57","192.168.8.165","127.0.0.1"]
+ALLOWED_HOSTS = ["fe62b4a6734b.ngrok-free.app","localhost","192.168.100.57","192.168.8.165","127.0.0.1"]
 
 # Application definition
 
