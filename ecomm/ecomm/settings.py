57745d14b0b63166dec578e4cca628ac6037cc4f
Henry Wanjiru <haronkuria2015@gmail.com>
2026-04-01 07:35:20 +0300

    refactores images display

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = True
+# DEBUG = env.bool("DEBUG", default=False)
 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
